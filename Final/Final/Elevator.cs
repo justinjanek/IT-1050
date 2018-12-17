0faa53e665fd1eeb160975007c2bf8211e8a1de1
Justin <justinjanek@ymail.com>
2018-12-17 00:50:05 -0500

    Progress to Final

---
+++ Final/Final/Elevator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Final
+{
+    public class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            this.Occupants[index] = new Passenger("",index);
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+
+            foreach (Passenger passenger in Occupants)
+            {
+                totalWeight += passenger.GetWeight();
+            }
+
+            return totalWeight;
+        } 
+
+        public bool IsOverMaxCapacity()
+        {
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Final/Final/Passenger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Final
+{
+    public class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public double GetWeight()
+        {
+            return this.Weight;
+        }
+    }
+}
